ed1b893309aafdc4e2098c5bcfa858349940c164
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-03-07 18:49:57 +0900

    go generate

---
+++ tool/gen.go
@@ -0,0 +1,47 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	p, err := exec.LookPath("go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Chdir(filepath.Dir(p))
+	b, err := exec.Command("git", "tag").CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf(strings.Join([]string{
+		`//go:generate go run tool/gen.go`,
+		``,
+		`package gover`,
+		``,
+		`var vers = [][2]string{`,
+		``}, "\n"))
+	for _, s := range strings.Split(string(b), "\n") {
+		if !strings.HasPrefix(s, "go") {
+			continue
+		}
+		if strings.Contains(s, "rc") || strings.Contains(s, "beta") {
+			continue
+		}
+		b, err = exec.Command("git", "log", "--format=%ci", fmt.Sprintf("%s^..%s", s, s)).CombinedOutput()
+		if err != nil {
+			continue
+		}
+		d := strings.TrimSpace(string(b))
+		fmt.Printf("	{`%s`, `%s`},\n", d, s)
+	}
+	fmt.Println("}")
+}
