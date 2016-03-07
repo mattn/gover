8a8d9e549b92a9004d792397aa6cb9dc23f16582
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-03-07 19:06:27 +0900

    Fixed test

--- ver_test.go
+++ ver_test.go
@@ -10,11 +10,15 @@ func TestSimple(t *testing.T) {
 	version := ""
 	func() {
 		defer func() {
-			recover()
-			paniced = true
+			if recover() != nil {
+				paniced = true
+			}
 		}()
 		version = Version()
 	}()
+	if paniced {
+		t.Fatal("maybe bug")
+	}
 	if !regexp.MustCompile(`^go\d+.\d+(.\d+)?$`).MatchString(version) {
 		t.Fatal("invalid version:", version)
 	}
